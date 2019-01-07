400f5b27ab85bfd141047412d593772207a45ac9
Trey Woods <46387468+twoods10123@users.noreply.github.com>
2019-01-06 21:07:27 -0500

    Add files via upload

---
+++ rlshader/__resource.lua
@@ -0,0 +1,5 @@
+resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+
+description 'A revive script by Woods'
+
+client_script 'client.lua'
---
+++ rlshader/client.lua
@@ -0,0 +1,4 @@
+AddEventHandler('onClientMapStart', function()
+	SetTimecycleModifier("v_dark")
+            SetTimecycleModifierStrength(0.3)
+end)
